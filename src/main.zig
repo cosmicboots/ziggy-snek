bc290a308eb367df2ac951b844fbf8b3a4db1f0e
Thom Dickson <td3of4@gmail.com>
2024-05-05 19:51:11 -0400

    Do some http body stuff

--- src/main.zig
+++ src/main.zig
@@ -1,26 +1,32 @@
 const std = @import("std");
 const log = std.log.scoped(.server);
 
-fn handleRequest(req: *std.http.Server.Request) !void {
+fn handleConnection(conn: std.net.Server.Connection, allocator: std.mem.Allocator) !void {
+    var buf: [4096]u8 = undefined;
+
+    var http = std.http.Server.init(conn, &buf);
+    var req = try http.receiveHead();
+
     log.info("Received request: {s}", .{req.head.target});
 
     var headers = req.iterateHeaders();
+    log.debug("=== HEADERS ===", .{});
     while (headers.next()) |header| {
-        log.info("{s}: {s}", .{ header.name, header.value });
+        log.debug("{s}: {s}", .{ header.name, header.value });
     }
+    log.debug("===============", .{});
+
+    var reader = try req.reader();
+    const body = try reader.readAllAlloc(allocator, std.math.maxInt(usize));
+    defer allocator.free(body);
+
+    log.debug("Recieved: [{s}]", .{body});
 
     const resp_headers: [1]std.http.Header = .{
         std.http.Header{ .name = "Content-Type", .value = "text/plain" },
     };
+
     try req.respond("Hello world", .{ .extra_headers = &resp_headers });
-}
-
-fn handleConnection(conn: std.net.Server.Connection) !void {
-    var buf: [1024]u8 = undefined;
-    var http = std.http.Server.init(conn, &buf);
-
-    var req = try http.receiveHead();
-    try handleRequest(&req);
 }
 
 fn runServer(server: *std.net.Server, allocator: std.mem.Allocator) !void {
@@ -30,7 +36,7 @@ fn runServer(server: *std.net.Server, allocator: std.mem.Allocator) !void {
         const conn = try server.accept();
         log.info("Accepted connection from {}", .{conn.address});
 
-        const handle = try std.Thread.spawn(.{}, handleConnection, .{conn});
+        const handle = try std.Thread.spawn(.{}, handleConnection, .{ conn, allocator });
         try threads.append(handle);
     }
 
@@ -39,12 +45,26 @@ fn runServer(server: *std.net.Server, allocator: std.mem.Allocator) !void {
     }
 }
 
+export fn sigHandler(sig: i32) void {
+    if (sig == std.os.linux.SIG.INT) {
+        log.info("Shutting down server...", .{});
+        std.os.linux.exit(0);
+    }
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
+    defer std.testing.expect(gpa.deinit() == .ok) catch unreachable;
     const allocator = gpa.allocator();
 
-    const address = std.net.Address.initIp4(.{ 127, 0, 0, 1 }, 3030);
-    var listener = try address.listen(.{});
+    const sigaction: std.os.linux.Sigaction = .{ .handler = .{ .handler = sigHandler }, .mask = .{0} ** 32, .flags = 0 };
+    _ = std.os.linux.sigaction(std.os.linux.SIG.INT, &sigaction, null);
+
+    var address = std.net.Address.initIp4(.{ 127, 0, 0, 1 }, 3030);
+
+    var listener: std.net.Server = try address.listen(.{ .reuse_address = true });
+    defer listener.deinit();
+
+    log.info("Listening on {}", .{address});
     try runServer(&listener, allocator);
 }
