a34268dffc79c8a4e5a252abfa8ae8f93d91def8
Thom Dickson <td3of4@gmail.com>
2024-05-05 15:31:49 -0400

    do some httpservering

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,50 @@
 const std = @import("std");
+const log = std.log.scoped(.server);
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+fn handleRequest(req: *std.http.Server.Request) !void {
+    log.info("Received request: {s}", .{req.head.target});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var headers = req.iterateHeaders();
+    while (headers.next()) |header| {
+        log.info("{s}: {s}", .{ header.name, header.value });
+    }
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    const resp_headers: [1]std.http.Header = .{
+        std.http.Header{ .name = "Content-Type", .value = "text/plain" },
+    };
+    try req.respond("Hello world", .{ .extra_headers = &resp_headers });
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn handleConnection(conn: std.net.Server.Connection) !void {
+    var buf: [1024]u8 = undefined;
+    var http = std.http.Server.init(conn, &buf);
+
+    var req = try http.receiveHead();
+    try handleRequest(&req);
+}
+
+fn runServer(server: *std.net.Server, allocator: std.mem.Allocator) !void {
+    var threads = std.ArrayList(std.Thread).init(allocator);
+
+    while (true) {
+        const conn = try server.accept();
+        log.info("Accepted connection from {}", .{conn.address});
+
+        const handle = try std.Thread.spawn(.{}, handleConnection, .{conn});
+        try threads.append(handle);
+    }
+
+    for (threads.items()) |thread| {
+        thread.join();
+    }
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const address = std.net.Address.initIp4(.{ 127, 0, 0, 1 }, 3030);
+    var listener = try address.listen(.{});
+    try runServer(&listener, allocator);
 }
